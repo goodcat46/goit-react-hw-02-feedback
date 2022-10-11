100931b05b285ce151350202d2bf58d01bba65a7
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-10-11 17:42:26 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,6 @@ export class App extends Component {
       <div className={css.app}>
         <FeedbackList
           toAddFeedback={this.toAddFeedback}
-          // feedbackListKeys={Object.keys(this.state)}
           state={this.state}
         />
       </div>
